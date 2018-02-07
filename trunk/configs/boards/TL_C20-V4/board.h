leksmax/padavan-fw

Add initial support fo TP-Link Archer C20V4

trunk/configs/boards/ARCHER-C20V4/board.h
@@ -1,0 +1,35 @@
+/* TP-LINK ARCHER C20 V4 */
+
+#define BOARD_PID		"ARCHER-C20V4"
+#define BOARD_NAME		"ARCHER-C20V4"
+#define BOARD_DESC		"TP-Link Archer C20 Wireless Router"
+#define BOARD_VENDOR_NAME	"TP-LINK Technologies CO., LTD."
+#define BOARD_VENDOR_URL	"http://www.tp-link.com/en"
+#define BOARD_MODEL_URL		"http://www.tp-link.com/en/download/Archer-C20_V4.html"
+#define BOARD_BOOT_TIME		25
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	38
+#define BOARD_GPIO_BTN_WPS	37
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_SW2G	39	/* soft led */
+#define BOARD_GPIO_LED_SW5G	40	/* soft led */
+#define BOARD_GPIO_LED_POWER	2
+#define BOARD_GPIO_LED_LAN	42
+#define BOARD_GPIO_LED_WAN	11
+#define BOARD_GPIO_LED_WAN_INVERTED
+#define BOARD_GPIO_LED_ISP	43
+#undef  BOARD_GPIO_LED_ISP_INVERTED
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB_ON
+#undef  BOARD_GPIO_PWR_USB
+#undef  BOARD_GPIO_PWR_USB2
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	1
+#define BOARD_NUM_ANT_5G_RX	1
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0

trunk/configs/boards/ARCHER-C20V4/board.mk
@@ -1,0 +1,8 @@
+##################################################################
+# Board PID # Board Name       # PRODUCT # Note
+##################################################################
+# ARCHERC20  # TP-LINK ARCHER C20    # MT7620  #
+##################################################################
+
+CFLAGS += -DBOARD_ARCHERC20V4 -DVENDOR_TPLINK
+BOARD_NUM_USB_PORTS=0

lekswrt/padavan-048

trunk/configs/boards/TL_C20-V4/board.h
@@ -1,0 +1,35 @@
+/* TP-LINK ARCHER C20 V4 */
+
+#define BOARD_PID		"TL_C20-V4"
+#define BOARD_NAME		"TL_C20-V4"
+#define BOARD_DESC		"TP-Link Archer C20 Wireless Router"
+#define BOARD_VENDOR_NAME	"TP-LINK Technologies CO., LTD."
+#define BOARD_VENDOR_URL	"http://www.tp-link.com/en"
+#define BOARD_MODEL_URL		"http://www.tp-link.com/en/download/Archer-C20_V4.html"
+#define BOARD_BOOT_TIME		25
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	38
+#define BOARD_GPIO_BTN_WPS	37
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_SW2G	39	/* soft led */
+#define BOARD_GPIO_LED_SW5G	40	/* soft led */
+#define BOARD_GPIO_LED_POWER	2
+#define BOARD_GPIO_LED_LAN	42
+#define BOARD_GPIO_LED_WAN	11
+#define BOARD_GPIO_LED_WAN_INVERTED
+#define BOARD_GPIO_LED_ISP	43
+#undef  BOARD_GPIO_LED_ISP_INVERTED
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB_ON
+#undef  BOARD_GPIO_PWR_USB
+#undef  BOARD_GPIO_PWR_USB2
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_NUM_ANT_5G_TX	1
+#define BOARD_NUM_ANT_5G_RX	1
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_HAS_EPHY_L1000	0
+#define BOARD_HAS_EPHY_W1000	0

trunk/configs/boards/TL_C20-V4/board.mk
@@ -1,0 +1,8 @@
+##################################################################
+# Board PID # Board Name       # PRODUCT # Note
+##################################################################
+# ARCHERC20  # TP-LINK ARCHER C20    # MT7620  #
+##################################################################
+
+CFLAGS += -DBOARD_ARCHER_C20 -DVENDOR_TPLINK
+BOARD_NUM_USB_PORTS=0