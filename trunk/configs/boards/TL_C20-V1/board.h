leksmax/padavan-fw

rename Archer C20 to Archer C20V1

trunk/configs/boards/ARCHER-C20V1/board.h
@@ -1,7 +1,7 @@
 /* TP-LINK ARCHER C20 V1 */
 
-#define BOARD_PID		"ARCHER-C20"
-#define BOARD_NAME		"ARCHER-C20"
+#define BOARD_PID		"ARCHER-C20V1"
+#define BOARD_NAME		"ARCHER-C20V1"
 #define BOARD_DESC		"TP-Link Archer C20 Wireless Router"
 #define BOARD_VENDOR_NAME	"TP-LINK Technologies CO., LTD."
 #define BOARD_VENDOR_URL	"http://www.tp-link.com/en"

trunk/configs/boards/ARCHER-C20V1/board.mk
@@ -4,5 +4,5 @@
 # ARCHERC20  # TP-LINK ARCHER C20    # MT7620  #
 ##################################################################
 
-CFLAGS += -DBOARD_ARCHERC20 -DVENDOR_TPLINK
+CFLAGS += -DBOARD_ARCHERC20V1 -DVENDOR_TPLINK
 BOARD_NUM_USB_PORTS=1

lekswrt/padavan-048

trunk/configs/boards/TL_C20-V1/board.h
@@ -1,7 +1,7 @@
 /* TP-LINK ARCHER C20 V1 */
 
-#define BOARD_PID		"ARCHER-C20"
-#define BOARD_NAME		"ARCHER-C20"
+#define BOARD_PID		"TL_C20-V1"
+#define BOARD_NAME		"TL_C20-V1"
 #define BOARD_DESC		"TP-Link Archer C20 Wireless Router"
 #define BOARD_VENDOR_NAME	"TP-LINK Technologies CO., LTD."
 #define BOARD_VENDOR_URL	"http://www.tp-link.com/en"

trunk/configs/boards/TL_C20-V1/board.mk
@@ -4,5 +4,5 @@
 # ARCHERC20  # TP-LINK ARCHER C20    # MT7620  #
 ##################################################################
 
-CFLAGS += -DBOARD_ARCHERC20 -DVENDOR_TPLINK
+CFLAGS += -DBOARD_ARCHER_C20 -DVENDOR_TPLINK
 BOARD_NUM_USB_PORTS=1